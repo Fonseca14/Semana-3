07dd47b29185803cbeb45b50443b4f45e1953ff6
Fonseca14 <56729210+Fonseca14@users.noreply.github.com>
2020-06-19 15:51:15 +0100

    Add files via upload

---
+++ JavaScript Semana 3.js
@@ -0,0 +1,74 @@
+//funçoes pre-definidas
+
+var elevado = Math.pow(8, 2);
+
+var raiz = Math.sqrt(64); 
+
+var arredondar = Math.round(4.7); 
+
+
+function arredondar(){
+	alert(arredondar);
+}
+
+function elevado(){
+	alert(elevado);
+}
+
+function raiz(){
+	alert(raiz);
+}
+
+function Array(){
+	var numMin = Math.min(... array);
+	alert(numMin);
+}
+
+//manipulaçao de strings
+
+function Substring(){
+	var string1 = "ola";
+	var string2 = "a";
+	var position = 4;
+
+}
+
+function toString(){
+	var string3 = 7;
+	alert(num.toString())
+}
+
+function toInteiro(){
+	var num = 7;
+	alert(parseint(string3))
+}
+
+function toString(){
+	var string3 = "3";
+	alert(parseint(string3))
+}
+
+function comprimentoString(){
+	var string5 = "Ola";
+alert(string5.length);
+}
+
+function posString(){
+	var string9 = "ola";
+	alert(string9.indexOf(ola));
+}
+
+function upperCase(){
+	var string10 = "ola";
+	alert(string10.toUpperCase());
+}
+
+function lowerCase(){
+	var string11 = "ola";
+	alert(string11.toLowerCase());
+}
+
+function gerarNum(){
+	var numGerado = Math.random;
+	alert(numGerado);
+}
